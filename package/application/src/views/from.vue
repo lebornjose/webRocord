b812ad79a15089da2f9f9aca1195b06a89c8e77c
黄兴 <huangxing@huangxingdeMacBook-Pro.local>
2025-04-21 22:37:15 +0800

    feat: 视频record

--- package/application/src/views/from.vue
+++ package/application/src/views/from.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="form-container">
   <div class="interaction rr-block">
-        <a-button type="primary" @click="record">录制</a-button>
+        <a-button type="primary" @click="recordPlay">录制</a-button>
         <a-button type="success" @click="replay">回放</a-button>
         <a-button type="warning" @click="reset">返回演示</a-button>
    </div>
@@ -26,29 +26,25 @@
   
 
   <div class="bottom">
-    <a-button type="primary">确定</a-button>
-    <a-button>取消</a-button>
+    <a-button type="primary" @click="onSubmit">打印表单数据</a-button>
+    <a-button type="primary" danger @click="logError">手动抛错</a-button>
   </div>
 
-  <div ref="replayer"></div>
+  <div ref="replayer" class="replay-container"></div>
 </div>
 </template>
 
 <script lang="js" setup>
 import { ref } from 'vue'
-import { record, getRecordConsolePlugin } from 'rrweb'
+import { message } from 'ant-design-vue';
+import { record, getRecordConsolePlugin, getReplayConsolePlugin } from 'rrweb'
 import rrwebPlayer from 'rrweb-player'
 
 const eventsMatrix = ref([[]])  // 使用二维数组来存放多个 event 数组
 const replayer = ref(null)
 const wrapperCol = { span: 14 };
 const labelCol = { style: { width: '150px' } };
-const formState = ref<{
-  name: string;
-  delivery: boolean;
-  type: string[];
-  desc: string;
-}>({
+const formState = ref({
   name: '',
   delivery: false,
   type: [],
@@ -56,7 +52,7 @@ const formState = ref<{
 });
 const stopFn = ref(null)
 
-const record = () => {
+const recordPlay = () => {
   console.log('录制');
   stopFn.value = record({
     checkoutEveryNth: 100, // 每 100 个 event 重新制作快照
@@ -84,23 +80,30 @@ const record = () => {
 const replay = () => {
   console.log('最近的操作记录: ', JSON.stringify(eventsMatrix.value[eventsMatrix.value.length - 1]));
   if(eventsMatrix.value[eventsMatrix.value.length - 1].length<=0) {
-    return this.$message.error("请先点击录制按钮进行录制！");
+    return message.error("请先点击录制按钮进行录制！");
   }
   stopFn.value()
   new rrwebPlayer({
         target: replayer.value, // 可以自定义 DOM 元素
         // 配置项
         props: {
-            logConfig: true,
-            events: eventsMatrix.value[eventsMatrix.value.length - 1],
-            plugins: [
-                getReplayConsolePlugin({
+          logConfig: true,
+          events: eventsMatrix.value[eventsMatrix.value.length - 1],
+          plugins: [
+            getReplayConsolePlugin({
                 level: ['info', 'log', 'warn', 'error'],
             }),
-        ],
+          ],
         },
     });
   console.log('回放');
+}
+
+const onSubmit = () => {
+      console.log(JSON.stringify(this.form))
+}
+const logError = () => {
+  throw new Error('手动抛错')
 }
 const reset = () => {
   console.log('返回演示');
@@ -108,6 +111,13 @@ const reset = () => {
 
 </script>
 <style lang="less" scope>
+.replayer-wrapper{
+  position: absolute;
+  top: -40%;
+  left: 20%;
+  width: 60%;
+  height: 100%;
+}
 .form-container{
   width: 660px;
   margin: 0 auto;
